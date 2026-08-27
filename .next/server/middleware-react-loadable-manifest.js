1150d27a4fe25fd1ceaa710b427a4449247b8915
Zeyad Waled <12345678+Zeyad-101@users.noreply.github.com>
2026-08-27 13:19:12 +0300

    Remove .next and node_modules from git tracking

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\page.js -> @/components/SceneCanvas\":{\"id\":\"app\\\\page.js -> @/components/SceneCanvas\",\"files\":[\"static/chunks/_app-pages-browser_components_SceneCanvas_jsx.js\"]}}"
